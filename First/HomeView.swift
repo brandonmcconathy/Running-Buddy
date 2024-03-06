dc26a2b77bbd62df16686f46213a8debb5a21d80
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-03-05 18:10:12 -0800

    Added slider and buttons for miles per week

--- First/HomeView.swift
+++ First/HomeView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var speed = 50.0
+    
     var body: some View {
-        VStack() {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Home Page")
+        VStack {
+            Text("Miles per week")
+                .font(.system(size: 28))
+            HStack {
+                Button {
+                    speed -= 1
+                } label: {
+                    Image(systemName: "minus")
+                }
+                    .font(.system(size: 25))
+                    .disabled(speed == 0 ? true : false)
+                Slider(
+                    value: $speed,
+                    in: 0...100,
+                    step: 1
+                )
+                    .padding(.horizontal, 5)
+                Button {
+                    speed += 1
+                } label: {
+                    Image(systemName: "plus")
+                }
+                    .font(.system(size: 25))
+                    .disabled(speed == 100 ? true : false)
+            }
+                .padding(.horizontal, 60)
+            Text("\(Int(speed))")
+                .font(.system(size: 20))
+                .padding(.vertical, 10)
+            Spacer()
         }
     }
+}
+
+#Preview {
+    ContentView()
 }
