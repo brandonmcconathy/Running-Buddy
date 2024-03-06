5531dd568ea88dce2521c5413dd1e2c027ce8f21
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-03-05 18:45:41 -0800

    added days per week and amounts to run

--- First/HomeView.swift
+++ First/HomeView.swift
@@ -9,38 +9,50 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State private var speed = 50.0
+    @State private var distance = 50.0
+    @State private var daysPerWeek = 5.0
     
     var body: some View {
         VStack {
-            Text("Miles per week")
+            Text("Running Schedule")
                 .font(.system(size: 28))
             HStack {
                 Button {
-                    speed -= 1
+                    distance -= 1
                 } label: {
                     Image(systemName: "minus")
                 }
                     .font(.system(size: 25))
-                    .disabled(speed == 0 ? true : false)
+                    .disabled(distance == 0 ? true : false)
                 Slider(
-                    value: $speed,
+                    value: $distance,
                     in: 0...100,
                     step: 1
                 )
                     .padding(.horizontal, 5)
                 Button {
-                    speed += 1
+                    distance += 1
                 } label: {
                     Image(systemName: "plus")
                 }
                     .font(.system(size: 25))
-                    .disabled(speed == 100 ? true : false)
+                    .disabled(distance == 100 ? true : false)
             }
                 .padding(.horizontal, 60)
-            Text("\(Int(speed))")
+            Text("Miles per week:  \(Int(distance))")
                 .font(.system(size: 20))
                 .padding(.vertical, 10)
+            Stepper(
+                value: $daysPerWeek,
+                in: 5...7,
+                step: 1
+            ) {
+                Text("Days per week:  \(Int(daysPerWeek))")
+            }
+            .padding(EdgeInsets(top: 10, leading: 50, bottom: 20, trailing: 50))
+            .font(.system(size: 20))
+            Text("Miles per day: \(distance * 0.75 / (daysPerWeek - 1))")
+            Text("Long Run: \(distance * 0.25)")
             Spacer()
         }
     }
